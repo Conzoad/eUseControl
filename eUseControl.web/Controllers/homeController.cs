bc17f1ba01f51b8d516f7cdaf11fcf153b18f2bc
suhom <suhom@CUMZOAD>
2024-02-14 14:34:58 +0200

    Add project files.

---
+++ eUseControl.web/Controllers/homeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eUseControl.web.App_Start
+{
+    public class homeController : Controller
+    {
+        // GET: home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
